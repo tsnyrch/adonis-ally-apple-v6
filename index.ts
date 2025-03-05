657a6bd3819070a93bafa0048049d4ea4f3890dc
Alexis DUMAIN <wailrothcs@gmail.com>
2025-03-05 17:51:49 +0100

    fix: configure, renaming the driver

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-export * from './src/driver.js'
+export { configure } from './configure.js'
+import type { HttpContext } from '@adonisjs/core/http'
+import type { AppleDriverConfig } from './src/apple.js'
+import { AppleDriver } from './src/apple.js'
+
+export function apple(config: AppleDriverConfig) {
+  return (ctx: HttpContext) => new AppleDriver(ctx, config)
+}
